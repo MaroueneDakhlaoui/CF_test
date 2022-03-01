5343d1483b3dd2113656e1ac26f7b11926db77ed
MaroueneDakhlaoui <marouenedakhlaoui@gmail.com>
2022-03-01 14:13:23 +0100

    cold

--- coffeeproject/coffee/settings.py
+++ coffeeproject/coffee/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'farms',
+    'coffeeproject.farms',
     'rest_framework',
     'rest_framework.authtoken',
 ]
