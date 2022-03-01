8cab381f45960e34dbb7542246cc6a3ac8a5c943
MaroueneDakhlaoui <marouenedakhlaoui@gmail.com>
2022-03-01 14:27:35 +0100

    wohoo

--- coffeeproject/farms/apps.py
+++ coffeeproject/farms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FarmsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coffeeproject.farms'
+    verbose_name = 'farms'
